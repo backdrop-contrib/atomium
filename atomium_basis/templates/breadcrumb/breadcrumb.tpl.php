e111dba6e9e53df0417c932d862b79920e9ee7b5
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-06-03 15:16:34 +0200

    Issue #423: Update breadcrumb component. (#424)
    * Issue #423: Update breadcrumb component.
    
    * Issue #423: Remove obsolete lines.
    
    * Issue #423: Remove obsolete line.
    
    * Issue #423: Update code style.

--- atomium_basis/templates/breadcrumb/breadcrumb.tpl.php
+++ atomium_basis/templates/breadcrumb/breadcrumb.tpl.php
@@ -5,7 +5,5 @@
  * Contains template file.
  */
 ?>
-<nav<?php print $atomium['attributes']['wrapper']->append('role', 'navigation')->append('class', 'breadcrumb'); ?>>
-  <h2 class="element-invisible"><?php print t('You are here'); ?></h2>
-  <?php print render($breadcrumb); ?>
-</nav>
+<h2 class="element-invisible"><?php print t('You are here'); ?></h2>
+<?php print render($breadcrumb); ?>
